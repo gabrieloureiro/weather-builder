0fd78d5a4888171ba49c2f5d8b99f2449b7eccc6
Gabriel Loureiro <gabrieloureirof@gmail.com>
2022-02-19 16:30:45 -0300

    feat: try to force msw interpect prod api calls

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,7 @@ import { AppProvider } from "providers";
 
 import "keen-slider/keen-slider.min.css";
 
-if (process.env.NEXT_PUBLIC_MOCK === "true") {
-  require("mocks/index");
-}
+require("mocks/index");
 
 const MyApp: React.VFC = ({ Component, pageProps }: AppProps) => {
   return (
